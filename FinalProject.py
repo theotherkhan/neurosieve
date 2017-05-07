a0704340d4caaf6b2f668dc3c1fcec24c3591686
Hasan Khan <theotherkhan@gmail.com>
2017-05-06 20:08:45 -0400

    working unet

--- FinalProject.py
+++ FinalProject.py
@@ -148,6 +148,7 @@ def get_groundTruths():
 
 gt = get_groundTruths()
 
+
 print (" \nExtraction Done!\n")
 
 
@@ -218,17 +219,25 @@ def make_model():
     conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool4)
     conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
 
+    print (" Conv6...\n")
+
     up6 = concatenate([UpSampling2D(size=(2, 2))(conv5), conv4], axis=3)
     conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(up6)
     conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv6)
+
+    print (" Conv7...\n")
 
     up7 = concatenate([UpSampling2D(size=(2, 2))(conv6), conv3], axis=3)
     conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(up7)
     conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv7)
 
+    print (" Conv8...\n")
+
     up8 = concatenate([UpSampling2D(size=(2, 2))(conv7), conv2], axis=3)
     conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(up8)
     conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv8)
+
+    print (" Conv9...\n")
 
     up9 = concatenate([UpSampling2D(size=(2, 2))(conv8), conv1], axis=3)
     conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(up9)
@@ -267,81 +276,62 @@ def resize_imgs(imgs):
 
 def train_and_predict():
 
-    train = allImages[0:5] #a list of 5, each element is a npy array
-    trainTruth = gt[0:5]   # both truth and train are the same size; checked
-
-    test = allImages[6:11]   
-    testTruth = gt[6:11]
-
-    trainS = []
-    trainTruthS = []
-    testS = []
-    testTruthS = []
-
-
     
-    print("\nExtracting one depth layer from each image...")
+    train = allImages[0:15] #a list of 5, each element is a npy array
+    trainTruth = gt[0:15]   # both truth and train are the same size; checked
+
+    test = allImages[15:]   
+    testTruth = gt[15:]
+
+    trainLayers = np.empty(shape=(15, 64, 64), dtype=float)
+    trainTruthLayers = np.empty(shape=(15, 64, 64), dtype=float)
+    testLayers = np.empty(shape=(15, 64, 64), dtype=float)
+    testTruthLayers = np.empty(shape=(15, 64, 64), dtype=float)
+
+    radius = 10
 
     for i in range (0, len(train)):
-        
-        depth = len(train[i])   
+        # Getting the fifth layer from each image in train, resizing it, converting to float32
+        midSlices = len(train[i])/2
+        for j in range(midSlices-radius, midSlices + radius):
+            np.append(trainLayers, resize(train[i][j][:][:], (x_ax, y_ax),preserve_range=True).astype('float32'))
+            np.append(trainTruthLayers,resize(trainTruth[i][j][:][:], (x_ax, y_ax),preserve_range=True).astype('float32'))
 
-        train[i] = train[i][(depth/2)][:][:]
-        trainTruth[i] = trainTruth[i][(depth/2)]
-        test[i] = test[i][(depth/2)]
-        testTruth[i] = testTruth[i][(depth/2)]
+    for i in range(0, len(test)):
+        midSlices = len(test[i])/2
+        for j in range(midSlices-radius, midSlices + radius):
+            np.append(testLayers,resize(test[i][j][:][:], (x_ax, y_ax),preserve_range=True).astype('float32'))
+            np.append(testTruthLayers,resize(testTruth[i][j][:][:], (x_ax, y_ax),preserve_range=True).astype('float32'))
 
-        # train, trainTruth, test, testTruth should now contain 5 2D slices each
+   
 
-    print (train[0].shape)
-    print ('')
-    print (train[0][0].shape)
-    print (train[0][0][0].shape)
-
-    '''
-
-    train = resize_imgs(train[0])
-
-
-    print ("Hello")
-    
-    for i in range (0, len(train)):
-
-        train = resize_imgs(train[i])
-        #trainTruth = resize_imgs(trainTruth[i])
-        #test = resize_imgs(test[i])
-        #testTruth = resize_imgs(testTruth[i])
-    
-
-    train = train.astype('float32')
-    trainTruth = trainTruth.astype('float32')
-    test = test.astype('float32')
-    testTruth = testTruth.astype('float32')
-
-    
+    trainLayers = trainLayers[..., np.newaxis]
+    trainTruthLayers = trainTruthLayers[..., np.newaxis]
+    testLayers = testLayers[..., np.newaxis]
+    testTruthLayers = testTruthLayers[..., np.newaxis]
     
     ############## Extra stuff!
 
-    
-    mean = np.mean(train)  # mean for data centering
-    std = np.std(train)  # std for data normalization
+    mean = np.mean(trainLayers)  # mean for data centering
+    std = np.std(trainLayers)  # std for data normalization
 
-    train -= mean
-    train /= std
+    trainLayers -= mean
+    trainLayers /= std
 
-    trainTruth /= 255.0  # scale masks to [0, 1]
+    trainTruthLayers /= 255.0  # scale masks to [0, 1]
 
     ##############
 
-    model = make_model()
+   # model = make_model()
+
+    
 
     print ("\nFitting...\n")
 
-    model.fit(train, trainTruth, batch_size=1, epochs=1, validation_data=(test, testTruth))
-    
-    #score = model.evaluate(x_test, y_test, verbose=0)
+    #model.fit(trainLayers, trainTruthLayers, batch_size=4, epochs=20, validation_data=(testLayers, testTruthLayers))
+    #score = model.evaluate(testLayers, testTruthLayers, verbose=0)
     #print ("\nDone! Score:", score)
-    '''
+    
 
 import time
 start_time = time.time()
